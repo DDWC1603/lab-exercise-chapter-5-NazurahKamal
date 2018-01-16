43960ad7d94a06a0c1ef6b0870d8e07576d9ab1e
NazurahKamal <33883971+NazurahKamal@users.noreply.github.com>
2018-01-16 11:47:55 +0800

    task complete

--- Exercise-Function-2.cpp
+++ Exercise-Function-2.cpp
@@ -1,9 +1,22 @@
-//this is an unfinish program
-//finish up this program
+#include<iostream>
+using namespace std;
 
-int sum(int x,int y)
+int add(int ,int);
+
+int main()
 {
-	int result;
-	result = x+y;
-
+	int num1, num2, sum;
+	cout<<"enter two numbers to sum= ";
+	cin>>num1 >>num2;
+	
+	sum = add(num1, num2);
+	cout<<"sum = " <<sum;
+	return 0;
+}
+int add(int x, int y)
+{
+	int add;
+	add = x + y;
+	
+	return add;
 }
